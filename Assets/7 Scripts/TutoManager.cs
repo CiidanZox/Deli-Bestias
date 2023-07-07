1781bd32ff1c2e36a6ba1a31e820d107209c9677
JoaquinMedel <j.medelplaza@uandresbello.edu>
2023-07-06 21:50:27 -0400

    Gary
    Finish te amo jose, buidea ty

--- Assets/7 Scripts/TutoManager.cs
+++ Assets/7 Scripts/TutoManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class TutoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> Tutorial;
+
+
+    public void Boton()
     {
-        
+        Tutorial[0].SetActive(false);
+        Tutorial[1].SetActive(true);
+        Tutorial[2].SetActive(false);
+        Tutorial[3].SetActive(false);
+        Tutorial[4].SetActive(false);
+        Tutorial[5].SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Boton1()
     {
-        
+        Tutorial[0].SetActive(false);
+        Tutorial[1].SetActive(false);
+        Tutorial[2].SetActive(true);
+        Tutorial[3].SetActive(false);
+        Tutorial[4].SetActive(false);
+        Tutorial[5].SetActive(false);
+    }
+    
+    public void Boton2()
+    {
+        Tutorial[0].SetActive(false);
+        Tutorial[1].SetActive(false);
+        Tutorial[2].SetActive(false);
+        Tutorial[3].SetActive(true);
+        Tutorial[4].SetActive(false);
+        Tutorial[5].SetActive(false);
+    }
+    
+    public void Boton3()
+    {
+        Tutorial[0].SetActive(false);
+        Tutorial[1].SetActive(false);
+        Tutorial[2].SetActive(false);
+        Tutorial[3].SetActive(false);
+        Tutorial[4].SetActive(true);
+        Tutorial[5].SetActive(false);
+    }
+    
+    public void Boton4()
+    {
+        Tutorial[0].SetActive(false);
+        Tutorial[1].SetActive(false);
+        Tutorial[2].SetActive(false);
+        Tutorial[3].SetActive(false);
+        Tutorial[4].SetActive(false);
+        Tutorial[5].SetActive(true);
+    }
+    
+    public void Boton5()
+    {
+        Tutorial[0].SetActive(true);
+        Tutorial[1].SetActive(false);
+        Tutorial[2].SetActive(false);
+        Tutorial[3].SetActive(false);
+        Tutorial[4].SetActive(false);
+        Tutorial[5].SetActive(false);
     }
 }
